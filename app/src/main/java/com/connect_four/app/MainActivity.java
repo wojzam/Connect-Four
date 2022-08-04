6ef43947657f3fbadc3b9e2cf37621164478c62b
wojzam <wojciech.zamarski@student.pk.edu.pl>
2022-08-04 11:10:07 +0200

    adapt to BoardView changes

--- app/src/main/java/com/connect_four/app/MainActivity.java
+++ app/src/main/java/com/connect_four/app/MainActivity.java
@@ -3,16 +3,31 @@ package com.connect_four.app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout boardLayout;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Board board = new Board();
-        BoardView boardView = new BoardView(board, this);
-        boardView.createViews();
+        boardLayout = findViewById(R.id.boardLayout);
+        text = findViewById(R.id.textView);
+        Button newGameButton = findViewById(R.id.newGameButton);
+        newGameButton.setOnClickListener(view -> createNewBoard());
+
+        createNewBoard();
+    }
+
+    private void createNewBoard() {
+        boardLayout.removeAllViews();
+        BoardView boardView = new BoardView(boardLayout, text);
+        boardView.createColumns();
     }
 }
