d04ebd88dcc635655ec0d69a73e89874a9baeecc
wojzam <wojciech.zamarski@student.pk.edu.pl>
2022-08-04 11:09:05 +0200

    add TextView to BoardView

--- app/src/main/java/com/connect_four/app/BoardView.java
+++ app/src/main/java/com/connect_four/app/BoardView.java
@@ -1,27 +1,53 @@
 package com.connect_four.app;
 
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+import static com.connect_four.app.Board.PLAYER_1;
+import static com.connect_four.app.Board.PLAYER_2;
 
 public class BoardView {
 
     private final Board board;
     private final LinearLayout layout;
+    private final TextView text;
 
-    public BoardView(Board board, LinearLayout layout) {
-        this.board = board;
+    public BoardView(@NonNull LinearLayout layout, @NonNull TextView text) {
+        this.board = new Board();
         this.layout = layout;
+        this.text = text;
     }
 
-    public void createViews() {
+    public void createColumns() {
         for (int i = 0; i < board.getWidth(); i++) {
             ColumnLayout columnLayout = new ColumnLayout(layout.getContext(), board.getHeight());
-            int finalJ = i;
+            int column = i;
             columnLayout.setOnClickListener(view -> {
-                board.insertIntoColumn(finalJ, Board.PLAYER_1);
-                columnLayout.refresh(board.getColumnValues(finalJ));
+                if (board.playerInsertIntoColumn(column)) {
+                    columnLayout.refresh(board.getColumnValues(column));
+                    updateTextView();
+                }
             });
 
             layout.addView(columnLayout);
+        }
+    }
+
+    private void updateTextView() {
+        switch (board.getCurrentPlayerID()) {
+            case PLAYER_1:
+                text.setText("Gracz 1");
+                text.setTextColor(ContextCompat.getColor(layout.getContext(), R.color.player1));
+                break;
+            case PLAYER_2:
+                text.setText("Gracz 2");
+                text.setTextColor(ContextCompat.getColor(layout.getContext(), R.color.player2));
+                break;
+            default:
+                text.setText("");
         }
     }
 }
