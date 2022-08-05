048f5c4b66d9c5207486d1d587722107a5d3eec8
wojzam <wojciech.zamarski@student.pk.edu.pl>
2022-08-05 17:24:09 +0200

    improve way of setting OnClickListener

--- app/src/main/java/com/connect_four/app/BoardView.java
+++ app/src/main/java/com/connect_four/app/BoardView.java
@@ -8,21 +8,15 @@ import java.util.ArrayList;
 
 public class BoardView extends LinearLayout {
 
-    private final Context context;
     private final Board board;
     private final ArrayList<ColumnLayout> columns = new ArrayList<>();
 
     public BoardView(Context context, Board board) {
         super(context);
-        this.context = context;
         this.board = board;
         setOrientation(HORIZONTAL);
         setGravity(Gravity.CENTER);
-        createColumns();
-    }
-
-    public ArrayList<ColumnLayout> getColumns() {
-        return columns;
+        createAndAddColumns(context);
     }
 
     public void refreshColumn(int column) {
@@ -35,9 +29,21 @@ public class BoardView extends LinearLayout {
         }
     }
 
-    public void createColumns() {
+    public void columnsSetOnClickListener(OnClickListener onClickListener) {
+        for(ColumnLayout columnLayout : columns){
+            columnLayout.setOnClickListener(onClickListener);
+        }
+    }
+
+    public void columnsRemoveOnClickListener() {
+        for(ColumnLayout columnLayout : columns){
+            columnLayout.setOnClickListener(null);
+        }
+    }
+
+    private void createAndAddColumns(Context context) {
         for (int i = 0; i < board.getWidth(); i++) {
-            ColumnLayout columnLayout = new ColumnLayout(context, board.getHeight());
+            ColumnLayout columnLayout = new ColumnLayout(context, board.getHeight(), i);
             addView(columnLayout);
             columns.add(columnLayout);
         }
