1d51266c5fbe1238baf029b49656954717c629c5
wojzam <wojciech.zamarski@student.pk.edu.pl>
2022-08-05 14:47:32 +0200

    add evaluate function for AI

--- app/src/main/java/com/connect_four/app/Board.java
+++ app/src/main/java/com/connect_four/app/Board.java
@@ -10,7 +10,6 @@ public class Board {
     private final byte[][] values;
     private final int width;
     private final int height;
-
     private byte currentPlayerID;
 
     public Board() {
@@ -96,6 +95,73 @@ public class Board {
             }
         }
         return false;
+    }
+
+    private int getSequenceScore(byte[] sequence, byte playerID) {
+        int score = 0;
+        int playerDiskCount = 0;
+        int emptyCount = 0;
+
+        for (byte value : sequence) {
+            if (value == playerID) {
+                playerDiskCount += 1;
+            } else if (value == EMPTY) {
+                emptyCount += 1;
+            }
+        }
+
+        if (playerDiskCount == 4) {
+            score += 100;
+        } else if (playerDiskCount == 3 && emptyCount == 1) {
+            score += 5;
+        } else if (playerDiskCount == 2 && emptyCount == 2) {
+            score += 2;
+        } else if (playerDiskCount == 0 && emptyCount == 1) {
+            score -= 4;
+        }
+        return score;
+    }
+
+    public int evaluate(byte playerID) {
+        byte[] sequence = new byte[4];
+        int score = 0;
+
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height - 3; j++) {
+                for (int k = 0; k < 4; k++) {
+                    sequence[k] = values[i][j + k];
+                }
+                score += getSequenceScore(sequence, playerID);
+            }
+        }
+
+        for (int i = 0; i < width - 3; i++) {
+            for (int j = 0; j < height; j++) {
+                for (int k = 0; k < 4; k++) {
+                    sequence[k] = values[i + k][j];
+                }
+                score += getSequenceScore(sequence, playerID);
+            }
+        }
+
+        for (int i = 0; i < width - 3; i++) {
+            for (int j = 0; j < height - 3; j++) {
+                for (int k = 0; k < 4; k++) {
+                    sequence[k] = values[i + k][j + k];
+                }
+                score += getSequenceScore(sequence, playerID);
+            }
+        }
+
+        for (int i = 3; i < width; i++) {
+            for (int j = 0; j < height - 3; j++) {
+                for (int k = 0; k < 4; k++) {
+                    sequence[k] = values[i - k][j + k];
+                }
+                score += getSequenceScore(sequence, playerID);
+            }
+        }
+        return score;
     }
 
     public boolean isColumnFull(int column) {
