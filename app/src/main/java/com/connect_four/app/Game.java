2881c5519a9be368e753e1e9dafed4a0b5c9c40e
wojzam <wojciech.zamarski@student.pk.edu.pl>
2022-08-09 12:13:00 +0200

    move front-end part of Game to GameViews

--- app/src/main/java/com/connect_four/app/Game.java
+++ app/src/main/java/com/connect_four/app/Game.java
@@ -1,54 +1,35 @@
 package com.connect_four.app;
 
-import android.view.Gravity;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import androidx.core.content.ContextCompat;
-
-import static com.connect_four.app.Board.PLAYER_1;
-import static com.connect_four.app.Board.PLAYER_2;
 
 public class Game {
 
-    private final BoardLayout boardLayout;
     private final Board board;
-    private final LinearLayout mainLayout;
-    private final TextView text;
+    private final GameViews gameViews;
     private boolean gameOver;
 
     public Game(LinearLayout mainLayout) {
         this.board = new Board();
-        this.mainLayout = mainLayout;
-        this.text = new TextView(mainLayout.getContext());
-        this.boardLayout = new BoardLayout(mainLayout.getContext(), board);
+        this.gameViews = new GameViews(mainLayout, board);
         this.gameOver = false;
-        arrangeViews();
+
+        gameViews.updateTextToDescribeBoardStatus();
     }
 
     public void restart() {
         board.resetBoard();
-        boardLayout.refresh();
-        boardLayout.columnsSetOnClickListener(view -> playerClickAction((ColumnLayout) view));
+        gameViews.getBoardLayout().refresh();
+        gameViews.getBoardLayout().columnsSetOnClickListener(view -> playerClickAction((ColumnLayout) view));
         gameOver = false;
 
-        updateText();
-    }
-
-    private void arrangeViews() {
-        mainLayout.setGravity(Gravity.CENTER);
-        text.setTextSize(30);
-        text.setGravity(Gravity.CENTER);
-
-        mainLayout.addView(boardLayout);
-        mainLayout.addView(text);
+        gameViews.updateTextToDescribeBoardStatus();
     }
 
     private void playerClickAction(ColumnLayout clickedColumn) {
         int column = clickedColumn.getIndex();
 
         if (board.insertIntoColumn(column)) {
-            boardLayout.refreshColumn(column);
+            gameViews.getBoardLayout().refreshColumn(column);
             finalizeTurn();
             if (!gameOver) {
                 aiTurn();
@@ -61,48 +42,21 @@ public class Game {
             endGame();
         } else if (board.isFull()) {
             endGame();
-            // TODO: This should better be inside updateText()
-            text.setText(R.string.tie_info);
-            text.setTextColor(ContextCompat.getColor(mainLayout.getContext(), R.color.white));
-            return;
         } else {
             board.changePlayer();
         }
-        updateText();
+        gameViews.updateTextToDescribeBoardStatus();
     }
 
     private void endGame() {
         gameOver = true;
-        boardLayout.columnsRemoveOnClickListener();
+        gameViews.getBoardLayout().columnsRemoveOnClickListener();
     }
 
     private void aiTurn() {
         int aiColumn = AI.chooseColumn(board);
         board.insertIntoColumn(aiColumn);
-        boardLayout.refreshColumn(aiColumn);
+        gameViews.getBoardLayout().refreshColumn(aiColumn);
         finalizeTurn();
-    }
-
-    private void updateText() {
-        switch (board.getCurrentPlayerID()) {
-            case PLAYER_1:
-                if (gameOver) {
-                    text.setText(R.string.player_1_won);
-                } else {
-                    text.setText(R.string.player_1_turn);
-                }
-                text.setTextColor(ContextCompat.getColor(mainLayout.getContext(), R.color.player1));
-                break;
-            case PLAYER_2:
-                if (gameOver) {
-                    text.setText(R.string.player_2_won);
-                } else {
-                    text.setText(R.string.player_2_turn);
-                }
-                text.setTextColor(ContextCompat.getColor(mainLayout.getContext(), R.color.player2));
-                break;
-            default:
-                text.setText("");
-        }
     }
 }
---
+++ app/src/main/java/com/connect_four/app/GameViews.java
@@ -0,0 +1,76 @@
+package com.connect_four.app;
+
+import android.content.Context;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.core.content.ContextCompat;
+
+import static com.connect_four.app.Board.PLAYER_1;
+
+public class GameViews {
+
+    private final Board board;
+    private final BoardLayout boardLayout;
+    private final TextView text;
+    private final int colorPlayer1;
+    private final int colorPlayer2;
+    private final int colorNeutral;
+
+    public GameViews(LinearLayout mainLayout, Board board) {
+        this.board = board;
+        this.text = new TextView(mainLayout.getContext());
+        this.boardLayout = new BoardLayout(mainLayout.getContext(), board);
+        this.colorPlayer1 = ContextCompat.getColor(mainLayout.getContext(), R.color.player1);
+        this.colorPlayer2 = ContextCompat.getColor(mainLayout.getContext(), R.color.player2);
+        this.colorNeutral = ContextCompat.getColor(mainLayout.getContext(), R.color.white);
+        arrangeViews(mainLayout);
+    }
+
+    public BoardLayout getBoardLayout() {
+        return boardLayout;
+    }
+
+    public void updateTextToDescribeBoardStatus() {
+        final int currentPlayerID = board.getCurrentPlayerID();
+        if (board.currentPlayerWonGame()) {
+            if (currentPlayerID == PLAYER_1) {
+                text.setText(R.string.player_1_won);
+                text.setTextColor(colorPlayer1);
+            } else {
+                text.setText(R.string.player_2_won);
+                text.setTextColor(colorPlayer2);
+            }
+        } else if (board.isFull()) {
+            text.setText(R.string.tie_info);
+            text.setTextColor(colorNeutral);
+        } else {
+            if (currentPlayerID == PLAYER_1) {
+                text.setText(R.string.player_1_turn);
+                text.setTextColor(colorPlayer1);
+            } else {
+                text.setText(R.string.player_2_turn);
+                text.setTextColor(colorPlayer1);
+            }
+        }
+    }
+
+    private void arrangeViews(LinearLayout mainLayout) {
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        layoutParams.setMargins(0, dpToPixel(30, mainLayout.getContext()), 0, 0);
+        text.setLayoutParams(layoutParams);
+        text.setTextSize(30);
+        text.setGravity(Gravity.CENTER);
+
+        mainLayout.addView(boardLayout);
+        mainLayout.addView(text);
+    }
+
+    private static int dpToPixel(float dp, Context context) {
+        return (int) (dp * ((float) context.getResources().getDisplayMetrics().densityDpi / DisplayMetrics.DENSITY_DEFAULT));
+    }
+}
