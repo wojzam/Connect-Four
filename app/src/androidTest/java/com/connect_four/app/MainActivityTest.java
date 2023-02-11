faf51ad15c959cb409c04a728a9ae6a5e0736529
wojzam <wojciech.zamarski@student.pk.edu.pl>
2023-02-11 12:43:11 +0100

    update MainActivityTest

--- app/src/androidTest/java/com/connect_four/app/MainActivityTest.java
+++ app/src/androidTest/java/com/connect_four/app/MainActivityTest.java
@@ -3,18 +3,19 @@ package com.connect_four.app;
 import android.widget.TextView;
 
 import androidx.appcompat.widget.AppCompatButton;
+import androidx.lifecycle.Lifecycle;
+import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
-import androidx.test.rule.ActivityTestRule;
 
 import com.connect_four.app.activities.MainActivity;
 import com.connect_four.app.model.Board;
 import com.connect_four.app.views.ColumnLayout;
 
 import org.junit.Before;
-import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.core.app.ActivityScenario.launch;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
@@ -29,13 +30,14 @@ import static org.hamcrest.Matchers.instanceOf;
 @RunWith(AndroidJUnit4.class)
 public class MainActivityTest {
 
-    @Rule
-    public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
-
     @Before
     public void setUp() {
-        Settings settings = new Settings(activityTestRule.getActivity());
-        settings.setSinglePlayer(false);
+        ActivityScenario<MainActivity> scenario = launch(MainActivity.class);
+        scenario.moveToState(Lifecycle.State.RESUMED);
+        scenario.onActivity(activity -> {
+            Settings settings = new Settings(activity);
+            settings.setSinglePlayer(false);
+        });
     }
 
     @Test
@@ -51,10 +53,27 @@ public class MainActivityTest {
     }
 
     @Test
-    public void shouldChangeTurn_whenTurnFinished() {
-        int index = 0;
-        onView(allOf(withTagValue(equalTo(index)), instanceOf(ColumnLayout.class))).perform(click());
+    public void shouldUpdateText_whenPlayerTurnChanged() {
+        onView(allOf(withTagValue(equalTo(0)), instanceOf(ColumnLayout.class))).perform(click());
+        waitForUpdate();
         onView(allOf(withText(R.string.player_2_turn), instanceOf(TextView.class))).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shouldUpdateText_whenPlayerTurnChangedTwice() {
+        onView(allOf(withTagValue(equalTo(0)), instanceOf(ColumnLayout.class))).perform(click());
+        waitForUpdate();
+        onView(allOf(withTagValue(equalTo(0)), instanceOf(ColumnLayout.class))).perform(click());
+        waitForUpdate();
+        onView(allOf(withText(R.string.player_1_turn), instanceOf(TextView.class))).check(matches(isDisplayed()));
+    }
+
+    private void waitForUpdate() {
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
